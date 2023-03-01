e4de3995d5f9efb279a87cd5b9867938179863b4
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-03-01 21:43:42 +0300

    добавлен response

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
+from requests import Response
 
+response = requests.get('https://api.github.com')
+
+# if response.status_code == 200:
+#     print('Success!')
+# elif response.status_code == 404:
+#     print('Not Found.')
+
+if response:
+    print('Success!')
+else:
+    print('An error has occurred.')
